2385e4bf593df95b7fa946bce25eeb0abcf3e514
rudolf-prog <rudolfwagner95@gmail.com>
2025-04-11 13:07:51 +0200

    11.04.25

---
+++ src/test/java/com/phonebook/test/CreateAccountTest.java
@@ -0,0 +1,34 @@
+package com.phonebook.test;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CreateAccountTest extends TestBase {
+    @Test
+    public void newUserRegistrationPositiveTest() {
+        driver.findElement(By.cssSelector("[href='/login']")).click();
+        //    enter email to email field
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys("qwe@bab.com");
+        //    enter password
+        driver.findElement(By.name("password")).click();
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("Aa123456$");
+        //    click on Registration button
+        driver.findElement(By.name("registration")).click();
+        //    verify SignOut button is displayed
+        Assert.assertTrue(isElementPresent(By.xpath("//button[.='Sign Out']")));
+
+
+    }
+//    click on login Linrk
+//    enter email to email field
+//    enter password
+//    enter Password to pasword field
+//    click on Registration button
+//    verify SignOut button is displayed
+
+
+}
